803c6a6274b7bd0ed5364cbd0153e29d17421084
Francisco Padilla Muñoz <padilla.fco@gmail.com>
2017-10-27 13:49:44 +0100

    fixed the loading animation

--- js/index.js
+++ js/index.js
@@ -2,17 +2,21 @@
 //GLOBAL VARIABLES
 var rootAPI= 'http://pokeapi.salestock.net/api/v2/';
 
-var cardTemplate ="<div id='{{id}}' class='cartaPokemon'>" +
+//TEMPLATES
+var tpmlCard = "<div id='{{id}}' class='cartaPokemon'>" +
 						"<figure class='containerImage'>" + 
 							"<img src='{{sprites.front_default}}'/>" +
 						"</figure>" + 
 						"<h2>Numero {{id}}</h2>" +
 					 	"<h1>I am: {{ name }}</h1>" +
 					"</div>";
+var tpmlLoading = "<div id='id' class='loadAnimation'>" +
+						"<img src='ajax-loader.gif'/>" +
+					"</div>";
 
 //HELPERS
-function displayCard (template, data) {
-	var html = Mustache.to_html(template, data);
+function displayTemplate (template, jsonData) {
+	var html = Mustache.to_html(template, jsonData);
 	$('#cardsBoard').append(html);
 }
 
@@ -20,6 +24,9 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function removeLoadAnimation() {
+	$('.loadAnimation').remove();
+}
 
 
 
@@ -37,14 +44,20 @@ function sendRequest(endpoint, successFunction) {
 						console.log("#beforeSend execute#" + '\n' +
 						 			"jqXHR is ", jqXHR,
 						 			"setting is ", settings);
+						
+						displayTemplate(tpmlLoading);
 						},
 		complete: function(jqXHR, textStatus) { 
 						console.log("#complete execute#" + '\n' +
 						 			"jqXHR is ", jqXHR,
 						 			"textStatus is ", textStatus);
+						removeLoadAnimation(endpoint);
 						},
-	}).done(function() {
+	}).done(function(done) {
+		console.log("done is ", done);
 		console.info("hecho")
+
+		
 	});
 };
 
@@ -80,25 +93,26 @@ $('#id_button').on("click", function() {
     //request.send(); // javascript request send
     //input = 1;
     sendRequest(endpoint, function (response) {
-    	displayCard(cardTemplate, response);
+    	displayTemplate(tpmlCard, response);
     });
 });
 
-var x = [];
 //Show 10 cards by type.
 $('#type_button').on("click", function() {
+	
 	var pokemonType = $('#type').val();
 	var endpointType = "type/" + pokemonType;
 
 	sendRequest(endpointType, function (response) {	
 		for (var i = 0; i < 10; i++) {
+
 			var randomIndex = getRandomInt(0, response.pokemon.length);
 			var name = response.pokemon[randomIndex].pokemon.name;
 
 			var endpointByName = "pokemon/" + name + "/";
 
 			sendRequest(endpointByName, function (response) {
-				displayCard(cardTemplate, response);
+				displayTemplate(tpmlCard, response);
 			})
 		}
 		
